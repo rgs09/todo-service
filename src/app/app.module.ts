13ff85fe3f4515251fba04f6ef304a196f157454
Roshan Shrivastava <gulla074@gmail.com>
2021-11-13 23:43:53 +0530

    spacex added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoDetailsComponent } from './todo-details/todo-details.component';
 import { Serve1Service } from './serve1.service';
+import { SpaceXComponent } from './space-x/space-x.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
-    TodoDetailsComponent
+    TodoDetailsComponent,
+    SpaceXComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [Serve1Service],
   bootstrap: [AppComponent]
